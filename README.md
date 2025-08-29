cd3e5e6aad4d99c897fe317f902605201d7ea131
james <james8197@live.com>
2025-08-29 21:50:34 +0930

    created empty tkinter window

--- README.md
+++
@@ -1,5 +0,0 @@
-# ImageSort
-
-This program sorts a folder of images (jpg, png, JPEG) by similarity.
-
-v0.0.1
---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+This is the main file to run the image sorting code from.
+
+'''
+
+import os
+from tkinter import *
+from tkinter import ttk
+
+# Create Tkinter window
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+root.geometry('900x600') # set default window size
+root.title('Image Sorter') # set title
+
+# Window header row
+
+
+
+root.mainloop()
